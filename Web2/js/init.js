9b53fb7fe388e4055cc4ec67a8a6ba38d7e83f7d
np <np@kood.com.ar>
2017-09-22 17:53:00 -0300

    web1 y web4

--- Web2/js/init.js
+++ Web2/js/init.js
@@ -1,7 +1,9 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
 
-    $('.button-collapse').sideNav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+      $('.button-collapse').sideNav();
+      $('.parallax').parallax();
+      $(document).ready(function() {
+        $('.parallax').parallax();
+      }); // end of document ready
+    })(jQuery); // end of jQuery name space

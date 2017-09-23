1559a43b09b66b12c3920d8e66cf03f2c6e548d1
np <np@kood.com.ar>
2017-09-23 14:09:29 -0300

    articulo .js mail footer

--- Web5/articulo.js
+++ Web5/articulo.js
@@ -1,5 +1,23 @@
-$(document).ready(()=>{
-  $.get('articulo.json', function(data){
-    debugger;
-  })
+$(document).ready(() => {
+  $.get('articulo.json', function(data) {
+    $(".titulo").html(data.titulo);
+    $(".main-text").html(data.descripcion);
+    $(".subtitulo").html(data.subtitulo);
+  });
+
+  $(".btn.btn-primary").on('click', (e) => {
+    e.preventDefault();
+    var email = $('#emailInput').val();
+    if (!email) {
+      $('#emailInput').val("Competar e-mail aca");
+    } else {
+      $.post('articulo.php', {
+          "email": email
+        },
+        function(data) {
+          alert("Enviado");
+        });
+    }
+
+  });
 });
